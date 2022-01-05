759899b7d64225b8d8ff0c6995b04f6357a84013
hilary3211 <80103905+hilary3211@users.noreply.github.com>
2022-01-05 20:49:04 +0100

    Add files via upload

---
+++ vote_coin_bot.py
@@ -0,0 +1,24 @@
+import discord #import all the necessary modules
+import os
+from neuralintents import GenericAssistant
+
+chatbot = GenericAssistant('intents2.json')
+chatbot.train_model()
+chatbot.save_model()
+
+print('bot done training')
+
+
+client = discord.Client()
+
+@client.event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+        
+    if message.content.startswith('Bot'):
+        response = chatbot.request(message.content[4:])
+        await message.channel.send(response)
+
+client.run('token')
